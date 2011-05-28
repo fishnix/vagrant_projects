b751159a08da7784f01c99972271240c9e855b63
E Camden Fisher <fish@fishnix.net>
2011-05-28 10:57:14 -0400

    s9ytest role change

--- centos64-s9ytest/roles/s9ydev.rb
+++ centos64-s9ytest/roles/s9ydev.rb
@@ -2,6 +2,7 @@ name "s9ydev"
 description "Role for s9y development"
 run_list(
   "recipe[apache2]",
+  "recipe[openssl]"
   "recipe[php::php5]",
   "recipe[php::module_mysql]",
   "recipe[mysql::server]",
